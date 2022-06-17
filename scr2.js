b6d1c1269ba686af9dd5cd64df48bb81a5396e38
Kirill Grishkov <foodsoli@mail.ru>
2022-06-17 12:37:01 +0300

    first commit

---
+++ scr2.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// alert("Hi!!!");
+
+// const resulta = confirm("go buhat'?")
+// y
+
+// const rusul = prompt("budesh' buhat'?", " ");
+// console.log(rusul);
+
+// const answers = [];
+
+// answers[0] = prompt('Ты человек?', "");
+// answers[1] = prompt('Ты робот?', "");
+// answers[3] = prompt('Ты собака?', "");
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+
+// const user = "Kirka";
+// alert(`Hello, ${user}`);
+// console.log(`http//supersite.com/${user}/esche/`);
+
+// const myName = "Kirill";
+// alert(myName);
+
+
+console.log(5 + + "4");
+
+let incr = 10,
+    decr = 11;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(5%2);
+
+console.log(2*4==9);
+
+const isCheked = true,
+      isClose = false;
+
+console.log(isCheked|| isClose);
---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const widthBox = 30;
+const lenghtBox = 50;
+console.log("Ширина коробки:" *widthBox * ',длина коробки: ' * lenghtBox );
+
+['sotrud', 'sotrud2', 'sotrud3'].map()
+
+//request
+//snake_case
+//UPPER_SNAKE_CASE
+//Kebab-case
+//PascalCase
+
+const userName = John;
+
+
+let number = 5;
+console.log(4/0);
+console.log('string'*9);
+
+const person = 'Kirill';
+
+const bool = true;
+
+let un;
+console.log(un);
+
+const obj = {
+  name: "Kirill",
+  age: 34,
+  profession: "masterChief"
+};
+
+console.log(obj.profession);
+
+
+let arr = ["foto.jpg", "foto2.jpeg", "foto3.bmp"];
+console.log(arr[0]);
+
+const obj = {
+  Kirill:1500,
+  Lesha: 3500,
+  Miha: "Loh"
+};
+console.log(obj.Kirill);
+
+const arr = ["Kira","Leha","Stas"];
+console.log(arr[0]);
+
+const objArr = {
+  0:'Kira',
+  1:"Leha",
+  2:"Stas"
+
+};
+objArr.c= "kino";
+
+console.log(objArr.c);
+
+const storeName = 'Sinyavka';
+const storeDescription = {
+  budget: 10000,
+  employees: ['Kira','Miha','Stas'],
+  products: {
+    'apple':10,
+    'orange':15
+  },
+open: true
+  
+};
